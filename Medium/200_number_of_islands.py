728f25beb1152d48cb89edf04f991318a86985a0
mattererer007 <mattretterer@tutanota.com>
2024-07-14 20:59:13 -0400

    Not Done Yet

---
+++ Medium/200_number_of_islands.py
@@ -0,0 +1,13 @@
+"""
+'0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ You may assume all four edges of the grid are all surrounded by water.
+
+"""
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        return int
+
+        # iterate through each row and count 1 
