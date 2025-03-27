b2ad0d25e1862dd7dbd35eae5fe38135f4a99cb5
mattretterer <matthew.m.retterer@parthenon.ey.com>
2025-03-26 20:16:45 -0400

    small input

---
+++ 0_Easy/14_longest_prefix.py
@@ -0,0 +1,20 @@
+from typing import List
+
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        longest_prefrix = strs[0]
+
+        return
+    
+if __name__ == "__main__":
+    strs = ["flower","flow","flight"]
+
+    solution = Solution()
+    print(solution.longestCommonPrefix(strs))
