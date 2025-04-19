c4369b990aa1f881ebc8be35253266b3bd35bf5e
mattererer007 <mattretterer@tutanota.com>
2025-04-18 22:41:43 -0400

    update

---
+++ 1_Medium/542_01_matrix.py
@@ -0,0 +1,11 @@
+from typing import List
+
+"""
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two cells sharing a common edge is 1.
+"""
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        return
