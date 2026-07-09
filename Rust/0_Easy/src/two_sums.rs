812221a770a7a45c90cb0e4850fc80f951380e76
mattererer007 <mattretterer@tutanota.com>
2026-07-08 22:02:24 -0400

    matts first rust problem

---
+++ Rust/0_Easy/src/two_sums.rs
@@ -0,0 +1,31 @@
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // i32 auto copies so can be referenced multiple times
+        
+        for (i, &number) in nums.iter().enumerate(){ // nums.iter() gives you: (usize, &i32)
+            // need the & to dereference it to the actual i32 for comparison            
+            let complement = target - number;
+            // .iter() borrows a vector enabling looping
+            for (j, &other_number) in nums.iter().enumerate(){
+                if complement == other_number && j != i {
+                    return vec![i as i32, j as i32]
+                }
+            }
+        }
+
+        vec![]
+    }
+}
+
+
+fn main() {
+
+    let nums = vec![1,2,10,5,-1];
+    let target = 7;
+
+    let result = Solution::two_sum(nums, target);
+    println!("{:?}", result);
+
+}
---
+++ Rust/target/debug/two_sums.d
@@ -0,0 +1,1 @@
+C:\Local\ Code\Local\ Github\Leet\ Code\Rust\target\debug\two_sums.exe: C:\Local\ Code\Local\ Github\Leet\ Code\Rust\0_Easy\src\two_sums.rs
